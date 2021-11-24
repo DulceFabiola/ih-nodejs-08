dd176c1a13356ff1a2fc9c0a4467bc40384ab49b
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-11-24 13:33:35 -0600

    validaciones

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,17 +12,52 @@ exports.register = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  //Validacion
+  //A) Verificar que username, email y password tengan contenido
+
+  if (!username || !email || !password) {
+    res.render("auth/signup", {
+      errorMessage: "Uno o mas campos estan vacios. Revisalos nuevamente",
+    });
+    return;
+  }
+
+  //B) Fortaleciemiento de password
+  //Verificar que password tengan 6 caracteres, un numero y una Mayuscula,
+
+  //expresiones regulares: permiten hacer validaciones,
+  //REGEX-Conjunto de reglas que auditan un texto plano
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.render("auth/signup", {
+      errorMessage:
+        "Tu password debe de contener 6 caracteres mínimo, un número y una mayúscula.",
+    });
+    return;
+  }
+
   //2.Ecriptacion de password
   //bcryptjs pide que antes de que encriptes tu password el server haga un revolvente de A-H y del 0-9
-  const salt = await bcryptjs.genSalt(10); //revolvente
-  const passwordEncriptado = await bcryptjs.hash(password, salt); //combinancion del texto plano con el revolvente
+  //
+  try {
+    const salt = await bcryptjs.genSalt(10); //revolvente
+    const passwordEncriptado = await bcryptjs.hash(password, salt); //combinancion del texto plano con el revolvente
 
-  const newUser = await User.create({
-    username,
-    email,
-    passwordEncriptado,
-  });
+    const newUser = await User.create({
+      username,
+      email,
+      passwordEncriptado,
+    });
 
-  //redireccion de usuario
-  res.redirect("/");
+    //redireccion de usuario
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    //Seccion de errores
+    //Error status: 500, error en el server
+    res.status(500).render("auth/signup", {
+      errorMessage:
+        "Hubo un error con la validacion de tu correo. Intenta nuevamente, nod ejes espacios, usa minusculas ",
+    });
+  }
 };
--- models/User.js
+++ models/User.js
@@ -4,7 +4,15 @@ const mongoose = require("mongoose");
 //2.SCHEMA
 const userSchema = mongoose.Schema({
   username: String,
-  email: String,
+  //Control de errores, en caso de que el usuario mande email vacio
+  email: {
+    type: String,
+    required: [true, "Email es requerido."], // QUE NO ESTÉ VACÍO
+    match: [/^\S+@\S+\.\S+$/, "Por favor, ingresa un email válido."], // REGEX DEL EMAIL
+    unique: true, // EMAIL ÚNICO EN LA BASE DE DATOS
+    lowercase: true, // MINÚSCULAS
+    trim: true, // SIN ESPACIOS VACÍOS
+  },
   passwordEncriptado: String,
 });
 
